4f4ea6768e7e7d21430c3fe606206715afad34e9
Sahariar <sahariaralam8@gmail.com>
2023-10-17 19:08:35 +0600

    final 2

--- src/component/Error.jsx
+++ src/component/Error.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Error = () => {
     return (
         <div className="bg-[url('/404.gif')] h-[600px] w-[800px] mt-20 bg-cover bg-center max-w-screen-xl mx-auto">
-            <NavLink to="/"><h1 className='font-rancho text-yellow-800 text-2xl text-center font-bold'>Go To home</h1></NavLink>
+            <NavLink to="/"><button className='font-rancho text-yellow-800 text-2xl mx-auto block px-4 py-2 rounded-lg font-bold bg-[#ECEAE3]'>Go To home</button></NavLink>
             
         </div>
     );
